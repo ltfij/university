1afa5de34d4ccebc085b6cb641ce13da3bbc55be
Henry J. Wylde <accounts+git@hjwylde.com>
2014-03-22 15:39:15 +1300

    Moved package structure for assignment01

---
+++ subprojects/swen430/src/main/java/com/hjwylde/uni/swen430/assignment01/whilelang/util/Attribute.java
@@ -0,0 +1,46 @@
+// This file is part of the WhileLang Compiler (wlc).
+//
+// The WhileLang Compiler is free software; you can redistribute
+// it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// The WhileLang Compiler is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with the WhileLang Compiler. If not, see
+// <http://www.gnu.org/licenses/>
+//
+// Copyright 2013, David James Pearce.
+
+package com.hjwylde.uni.swen430.assignment01.whilelang.util;
+
+public interface Attribute {
+
+    public static class Source implements Attribute {
+
+        public final int start;
+        public final int end;
+
+        public Source(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public String toString() {
+            return "@" + start + ":" + end;
+        }
+    }
+
+    public static class Type implements Attribute {
+
+        public final com.hjwylde.uni.swen430.assignment01.whilelang.lang.Type type;
+
+        public Type(com.hjwylde.uni.swen430.assignment01.whilelang.lang.Type type) {
+            this.type = type;
+        }
+    }
+}
